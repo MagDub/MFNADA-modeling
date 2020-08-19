c7f4d1726a29ac1cb2d7c9e8092e676a1257a973
Magda Dubois <magda.dubois.18@ucl.ac.uk>
2020-08-19 18:11:39 +0200

    fit

---
+++ fit/main.m
@@ -0,0 +1,9 @@
+
+addpath('./holly/')
+data_fol = ('../../data');
+
+for ID=501
+        
+    fit_mod12_like_param_recovery_2sgm0_prior1normal(ID, data_fol)
+    
+end
--- running_.m
+++ running_.m
@@ -1,3 +1,3 @@
 
  
-for ID=501:560; fit_mod12_like_param_recovery(ID); end
+for ID=501:560; fit_mod12_like_param_recovery_2sgm0_prior1normal(ID); end
